159a8de1910b2aefa60f884f1b06ccaaeaff1acb
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-05-08 11:03:05 +0800

    Rename Example Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,11 @@ var React = require("react");
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component</h3>
+        <div>
+            <h3>Examples</h3>
+            <p>Welcome to the Examples Page!</p>
+        </div>        
+        
     )
 };
 
